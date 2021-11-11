e6034f3e21cb8b6adcc5559496dd40718c40a37f
Chris Golding <chris.golding@gmail.com>
2021-11-10 21:56:45 -0500

    Initial Commit

---
+++ Sport Scores/Sport_ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sport_ScoresApp.swift
+//  Sport Scores
+//
+//  Created by Chris on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Sport_ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
